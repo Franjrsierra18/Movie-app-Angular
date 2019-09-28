0923c54c452933c52b19d0ae001023f01b5dfef4
Francisco Sierra <franjrsierra18@gmail.com>
2019-09-28 23:10:43 +0200

    added another service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { TvPopularComponent } from './components/tv-popular/tv-popular.component';
+import { MainComponent } from './components/main/main.component';
 
 
 const routes: Routes = [
+  { path: '', component: MainComponent },
   { path: 'tv/:category', component: TvPopularComponent },
   { path: 'movies/:category', component: MoviesComponent },
   { path: 'movie/:id', component: MovieDetailComponent }
---
+++ src/app/services/people.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+  constructor(public http: HttpClient) { }
+
+  getSeries(category: string, language: string, page: string): Observable<object> {//creamos tantos métodos como necesitemos para traer la información que requieren nuestros componentes
+    return this.http.get(`https://api.themoviedb.org/3/tv/${category}?api_key=09e6cf9871795d48bba7c9be1465ff3e&language=${language}&page=${page}`)
+  }
+}
